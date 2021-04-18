ed82c22c405c64d8578f1c8e0bc39bbd2607dbb2
sisisin <azsisisin@gmail.com>
2021-04-18 18:00:11 +0900

    configure redis

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # try-heroku
+
+heroku create try-sisisin-heroku --manifest
+heroku stack:set heroku-20
+git push heroku main
+
+heroku apps:destroy
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const http = require('http');
+const redis = require('redis');
+const redisClient = redis.createClient(process.env.REDIS_URL);
+
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hello World${process.env.FOO_BAR}`);
+  const d = Date.now();
+  redisClient.SET('the_key', `time: ${d.toString()}`, (err, y) => {
+    res.setHeader('Content-Type', 'text/plain');
+    if (err) {
+      res.statusCode = 500;
+      res.end('err!');
+      return;
+    }
+
+    res.statusCode = 200;
+    res.end(`Hello World${process.env.FOO_BAR}`);
+  });
 });
 
 server.listen(port, () => {
